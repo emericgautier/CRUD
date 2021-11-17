bb67539fe0bc6c528419726eae07743a695f2d1b
emericgautier <emericgtr@gmail.com>
2021-11-17 16:39:28 +0100

    CORS

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,13 @@ mongoose.connect('mongodb+srv://Emeric:ygNtmCA9z2qiTRIG@cluster0.gp1ym.mongodb.n
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 app.use(bodyParser.json());
 
 app.post('/api/products', (req, res, next) => {
