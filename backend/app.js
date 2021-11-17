510d33971ad0d98426f72eeddd9ce3e74423c645
emericgautier <emericgtr@gmail.com>
2021-11-17 17:35:12 +0100

    Product updated

--- backend/app.js
+++ backend/app.js
@@ -42,5 +42,11 @@ mongoose.connect('mongodb+srv://Emeric:ygNtmCA9z2qiTRIG@cluster0.gp1ym.mongodb.n
         .then(product => res.status(200).json({product}))
         .catch(error => res.status(404).json({ error }));
     });
+
+    app.put('/api/products/:id', (req, res, next) => {
+        Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+          .catch(error => res.status(400).json({ error }));
+    });
     
     module.exports = app;
