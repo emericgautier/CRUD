5447544319076b8fedf7e2ea169c4ef256e30db4
emericgautier <emericgtr@gmail.com>
2021-11-17 17:34:16 +0100

    Found individual product

--- backend/app.js
+++ backend/app.js
@@ -7,34 +7,40 @@ const Product = require('./models/product');
 const app = express();
 
 mongoose.connect('mongodb+srv://Emeric:ygNtmCA9z2qiTRIG@cluster0.gp1ym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
+{ useNewUrlParser: true,
     useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
-
-app.use(bodyParser.json());
-
-app.post('/api/products', (req, res, next) => {
-    delete req.body._id;
-    const product = new Product({
-      ...req.body
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+    
+    app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+        next();
     });
-    product.save()
-      .then(() => res.status(201).json({product}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-  app.get('/api/products', (req, res, next) => {
-    Product.find()
-      .then(products => res.status(200).json({products}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-module.exports = app;
+    
+    app.use(bodyParser.json());
+    
+    app.post('/api/products', (req, res, next) => {
+        delete req.body._id;
+        const product = new Product({
+            ...req.body
+        });
+        product.save()
+        .then(() => res.status(201).json({product}))
+        .catch(error => res.status(400).json({ error }));
+    });
+    
+    app.get('/api/products', (req, res, next) => {
+        Product.find()
+        .then(products => res.status(200).json({products}))
+        .catch(error => res.status(400).json({ error }));
+    });
+    
+    app.get('/api/products/:id', (req, res, next) => {
+        Product.findOne({ _id: req.params.id })
+        .then(product => res.status(200).json({product}))
+        .catch(error => res.status(404).json({ error }));
+    });
+    
+    module.exports = app;
